023b0cf716480b1aa08d9721a1fc64c55eba74fe
ParanC <paranchoudhury05@gmail.com>
2026-05-24 20:09:05 +0530

    Code Changes done to Update student and delete student

--- main2.py
+++ main2.py
@@ -135,27 +135,50 @@ while True:
 
             #update file after deletion
             with open("students.txt","w") as file:
-                for student_name,student_marks in student.items():
-                    file.write(f"{student_name},{student_marks}\n")
+                for student_name, subjects in student.items():
+                    file.write(
+                        f"{student_name},"
+                        f"{subjects['Math']},"
+                        f"{subjects['English']},"
+                        f"{subjects['Science']},"
+                        f"{subjects['Social Science']},"
+                        f"{subjects['Computer']},"
+                        f"{subjects['Assamese']}\n"
+                    )
             print(f"{del_name} deleted successfully!")
         else:
             print("Student not found!")
     # 5. Update student marks
     elif choice == "5":
-        update_name = input("Enter the name pf the student to update marks: ")
+        update_name = input("Enter the name of the student: ")
         if update_name in student:
-            try:
-                new_marks =int(input("Enter the new marks: "))
-                student[update_name] = new_marks
-                #Rewrite updated data into file
-                with open("students.txt","w") as file:
-                    for student_name,student_marks in student.items():
-                        file.write(f"{student_name},{student_marks}\n")
-                print(f"{update_name}'s marks updated successfully!")
-            except ValueError:
-                print(" Invalid marks input! ")
+            subject = input("Enter the subject to update: ")
+            if subject in student[update_name]:
+                try:
+                    new_marks = int(input("Enter the new marks: "))
+
+                    #Update only selected subject marks
+                    student[update_name][subject] = new_marks
+                    #Rewrite updated data into file
+                    with open("students.txt","w") as file:
+                        for student_name, subjects in student.items():
+                            file.write(
+                                f"{student_name},"
+                                f"{subjects['Math']},"
+                                f"{subjects['English']},"
+                                f"{subjects['Science']},"
+                                f"{subjects['Social Science']},"
+                                f"{subjects['Computer']},"
+                                f"{subjects['Assamese']}\n"
+                            )
+                    print(f"{update_name}'s {subject} marks updated successfully")
+                except ValueError:
+                    print(" Invalid marks input! ")
+            else:
+                print(" Subject not found! ")
         else:
-            print("Student not found!")
+            print("Student not found! ")
+
     # 6 . Search Student
     elif choice == "6":
 
